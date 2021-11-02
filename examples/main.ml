457d7c6471837c7bb26deb1c8d534228f8a597c7
mamewotoko <mamewotoko@users.noreply.github.com>
2021-11-02 23:15:53 +0000

    deploy: 74be9487955590d60339b2d48556b4450189d815

---
+++ examples/main.ml
@@ -0,0 +1,29 @@
+open Webapi
+open Babylonjs
+
+exception Not_found_error of string
+
+let canvas_id = "renderCanvas"
+
+let _ =
+  match Dom.Document.getElementById canvas_id Dom.document with
+    None -> raise (Not_found_error "canvas element is not found")
+  | Some canvas ->
+     let engine = Engine.(make canvas true
+                            (param
+                               ~preserveDrawingBuffer:true
+                               ~stencil:true
+                               ())) in
+     let scene = Scene.make engine in
+     let v =  Vector3.make 0.0 5.0 (~-.10.0) in
+     let camera = FreeCamera.make "camera1" v scene in
+     let _ = HemisphericLight.make "light1" (Vector3.make 0.0 1.0 1.0) scene in
+     let sphere = MeshBuilder.(createSphere "sphere1"
+                                 (param
+                                    ~segments:16
+                                    ~diameter:2
+                                    ())
+                                 scene) in
+     sphere ## position ## y #= 1.0;
+     camera ## setTarget (Vector3.make 0.0 0.0 0.0);
+     engine ## runRenderLoop (fun () -> scene ## render ());
